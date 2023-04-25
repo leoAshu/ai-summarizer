1826158383d1dc7a378833d35a6e7ae132335c8b
leoAshu <ashutosh.ojha2009@gmail.com>
2023-04-24 20:55:49 -0700

    designs demo section

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+import { copy, linkIcon, loader, tick } from '../assets';
 
 function Demo() {
     return (
-        <div>Demo</div>
+        <section className='mt-16 w-full max-w-xl'>
+            {/* Search */}
+            <div className='flex flex-col w-full gap-2'>
+                <form 
+                    className='relative flex justify-center items-center'
+                    onSubmit={() => {}}
+                >
+                    <img 
+                        src={linkIcon}
+                        alt='link icon'
+                        className='absolute left-0 my-2 ml-3 w-5'
+                    />
+
+                    <input 
+                        type='url'
+                        placeholder='Enter a URL'
+                        value=''
+                        onChange={() => {}}
+                        required
+                        className='url_input peer'
+                    />
+
+                    <button
+                        type='submit'
+                        className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'
+                    >
+                        ↵
+                    </button>
+                </form>
+
+                {/* URL History */}
+            </div>
+
+            {/* Display Results */}
+        </section>
     );
 }
 
