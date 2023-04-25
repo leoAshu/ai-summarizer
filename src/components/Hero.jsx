554c1e3dd2765bafc749ef3b030ae40c1d530ef0
leoAshu <ashutosh.ojha2009@gmail.com>
2023-04-24 20:45:15 -0700

    builds and designs hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 
+import { logo } from '../assets';
+
 function Hero() {
     return (
-        <div>Hero</div>
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className='flex justify-between items-center flex-row w-full mb-10 pt-3'>
+                <img 
+                    src={logo} 
+                    alt='sumz logo' 
+                    className='w-28 object-contain'
+                />
+
+                <button
+                    type='button'
+                    onClick={() => window.open('https://github.com/leoAshu')}
+                    className = 'black_btn'
+                >
+                    GitHub
+                </button>
+            </nav>
+
+            <h1 className='head_text'>
+                Summarize Articles with <br className='max-md:hidden' />
+                <span className='orange_gradient'>OpenAI GPT-4</span>
+            </h1>
+
+            <h2 className='desc'>
+                Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries
+            </h2>
+        </header>
     );
 }
 
